3a46cf737052c74e3a2dfc025226cd95b93b8cfa
chinsh1 <90356128+chinsh1@users.noreply.github.com>
2022-08-08 12:35:15 +1200

    Create index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+include 'connection.php';
+//link to connection page
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- title of page in link-->
+    <title> Ice Cream </title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+<header>
+    <!-- title of page -->
+    <h1> Ice Cream </h1>
+</header>
+
+<nav>
+    <!-- links to other pages -->
+    <a href="index.php"> home </a>
+    <a href="flavours.php"> flavours </a>
+    <a href="order.php"> order </a>
+</nav>
+
+<main>
+    <h2> About Us</h2>
+    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem
+        aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo
+        enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
+        qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
+        consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam.
+    </p>
+
+    <h2> Open Hours </h2>
+    <!-- when shop is open -->
+    <p> open 24/7</p>
+</main>
