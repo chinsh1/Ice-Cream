39a57413494d9307330795833911abae17eab823
chinsh1 <90356128+chinsh1@users.noreply.github.com>
2022-08-08 12:35:44 +1200

    Create flavours.php

---
+++ flavours.php
@@ -0,0 +1,30 @@
+<?php
+include 'connection.php';
+//link to connection page
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- title of page in link-->
+    <title> Ice Cream </title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+<header>
+    <!-- title of page -->
+    <h1> Ice Cream </h1>
+</header>
+
+<nav>
+    <!-- links to other pages -->
+    <a href="index.php"> home </a>
+    <a href="flavours.php"> flavours </a>
+    <a href="order.php"> order </a>
+</nav>
+
+<main>
+
+</main>
