eca40876aeb5c99c2ddd24e695cb775850bb185a
chinsh1 <90356128+chinsh1@users.noreply.github.com>
2022-09-01 13:57:31 +1200

    Update flavours.php

--- flavours.php
+++ flavours.php
@@ -5,7 +5,7 @@ $con = mysqli_connect("localhost", "chinsh", "swiftbrain55", "chinsh_icecream");
 
 /* Flavours Query*/
 /* SELECT FlavourID, Name FROM flavours */
-$all_flavours_query = 'SELECT FlavourID, Name FROM flavours';
+$all_flavours_query = 'SELECT FlavourID, Name, Cost, Stock FROM flavours';
 $all_flavours_result = mysqli_query($con, $all_flavours_query);
 ?>
 
@@ -32,18 +32,65 @@ $all_flavours_result = mysqli_query($con, $all_flavours_query);
 </nav>
 
 <main>
-    <!-- flavours form -->
-    <form name="flavours_form" id="flavours_form" method="get" action="flavours.php">
-        <select id="flavour" name="flavour">
-            <!--options-->
-            <?php
-            while($all_flavours_record = mysqli_fetch_assoc($all_flavours_result)) {
-                echo "<option value = '". $all_flavours_record['FlavourID'] . "'>";
-                echo $all_flavours_record['Name'];
-                echo "</option>";
-            }
-            ?>
-        </select>
-        <input type="submit" name="flavour_button" value="Show me the flavours">
+    <!-- search bar for all food -->
+    <h2>Search a Item</h2>
+    <form action="flavours.php" method="post">
+        <input type="text" name='search'>
+        <input type="submit" name="submit" value="go!">
     </form>
+    <!-- php for search bar -->
+    <?php
+
+    if (isset($_POST['search'])) {
+        $search = $_POST['search'];
+        // query to find item that is searched from database
+        $query1 = "SELECT * FROM flavours WHERE Name LIKE '%$search%'";
+        $query = mysqli_query($con, $query1);
+        $count = mysqli_num_rows($query);
+        //if input is invalid it will print this message
+        if ($count == 0) {
+            echo "<p> There was no search results!";
+        } else {
+
+            while ($row = mysqli_fetch_array($query)) {
+                echo "<br>";
+                echo $row ['Name'];
+            }
+        }
+    }
+    ?>
+    <!-- button that shows all flavours in alphabetical order asc-->
+    <form action="flavours.php" method="post" class="button">
+        <input type='submit' name='allflavoursquery' value="All Flavours">
+    </form>
+
+    <?php
+    if (isset($_POST['allflavoursquery'])) {
+    // query that prints by ID number
+        $result = mysqli_query($con, "SELECT * FROM `flavours` ORDER BY `flavours`.`Name` ASC");
+        if (mysqli_num_rows($result) != 0) {
+            echo "<table>";
+            echo "<thead>";
+            echo "<tr>";
+            echo "<th>FLAVOUR ID</th>";
+            echo "<th>FLAVOUR</th>";
+            echo "<th>COST </th>";
+            echo "<th>STOCK</th>";
+            echo "</tr>";
+            echo "</thead>";
+            while ($test = mysqli_fetch_array($result)) {
+                $id = $test['FlavourID'];
+                //titles of each column
+                echo "<tr>";
+                echo "<td>" . $test['FlavourID'] . "</td>";
+                echo "<td>" . $test['Name'] . "</td>";
+                echo "<td>" . $test['Cost'] . "</td>";
+                echo "<td>" . $test['Stock'] . "</td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+        }
+    }
+    ?>
+
 </main>
