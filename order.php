5b49cfd2b616712b15b3ef97650201a938be91cb
chinsh1 <90356128+chinsh1@users.noreply.github.com>
2022-09-01 13:58:09 +1200

    Update order.php

--- order.php
+++ order.php
@@ -1,6 +1,15 @@
 <?php
+$con = mysqli_connect("localhost", "chinsh", "swiftbrain55", "chinsh_icecream");
 include 'connection.php';
 //link to connection page
+
+/* Order Query*/
+/* SELECT OrderID, CustomerID FROM orders*/
+
+/* Flavours Query*/
+/* SELECT FlavourID, Name FROM flavours */
+$all_flavours_query = 'SELECT FlavourID, Name, Cost, Stock FROM flavours';
+$all_flavours_result = mysqli_query($con, $all_flavours_query);
 ?>
 
 <!DOCTYPE html>
@@ -26,5 +35,61 @@ include 'connection.php';
 </nav>
 
 <main>
-    
+    <!-- using a form to insert an entry into the database -->
+    <h2>Add Customer</h2>
+    <form action="insertcustomer.php" method="post">
+        <!-- enter first name -->
+        <label for="FName">First Name: </label><br>
+        <input type="text" id="FName" name="FName"><br>
+        <!-- enter last name -->
+        <label for="LName">Last Name: </label><br>
+        <input type="text" id="LName" name="LName"><br>
+        <!-- enter address -->
+        <label for="Address">Address: </label><br>
+        <input type="text" id="Address" name="Address"><br>
+        <!-- enter phone number -->
+        <label for="Phone">Phone Number: </label><br>
+        <input type="text" id="Phone" name="Phone"><br>
+        <!-- enter email address -->
+        <label for="Email">Email: </label><br>
+        <input type="text" id="Email" name="Email"><br>
+        <!-- submit button-->
+        <input type="submit" value="submit">
+    </form>
+
+
+    <!--
+    <label for="cost">Cost:</label><br>
+    <input type="text" id="cost" name="cost"><br>
+    -->
+    <h2>Add Order</h2>
+    <form action="insertorder.php" method="post">
+
+        <!-- enter customer ID -->
+        <label for="CustomerID">Customer ID: </label><br>
+        <input type="text" id="CustomerID" name="CustomerID"><br>
+
+        <!-- enter Flavour ID -->
+        <label for="flavour">Flavour: </label><br>
+        <select id="flavour" name="flavour">
+            <!--options-->
+            <?php
+            while($all_flavours_record = mysqli_fetch_assoc($all_flavours_result)) {
+                echo "<option value = '". $all_flavours_record['FlavourID'] . "'>";
+                echo $all_flavours_record['Name'];
+                echo "</option>";
+            }
+            ?>
+        </select><br>
+
+
+        <!-- enter quantity  -->
+        <label for="Quantity">Quantity: </label><br>
+        <input type="text" id="Quantity" name="Quantity"><br>
+
+        <!-- submit button-->
+        <input type="submit" value="submit">
+
+    </form>
+
 </main>
