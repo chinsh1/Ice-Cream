f71fbcfaef8074c1d689f8b2b83089ef4024d654
chinsh1 <90356128+chinsh1@users.noreply.github.com>
2022-09-14 18:07:55 +1200

    Update update.php

--- update.php
+++ update.php
@@ -1,9 +1,13 @@
 <?php
 $con = mysqli_connect("localhost", "chinsh", "swiftbrain55", "chinsh_icecream");
-include 'connection.php';
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL:" . mysqli_connect_error();
+    die();
+} else {
+    echo "connected to database";
+}
 
-$update_order = "UPDATE orders SET CustomerID='$_POST[CustomerID]',Flavour = '$_POST[FlavourID]', 
-                  Quantity='$_POST[Quantity]', WHERE OrderID='$_POST[OrderID]'";
+$update_order = "UPDATE orders SET CustomerID='$_POST[CustomerID]',FlavourID = '$_POST[FlavourID]',Quantity = '$_POST[Quantity]' WHERE OrderID='$_POST[OrderID]'";
 
 /* Check the data has been updated */
 if(!mysqli_query($con, $update_order))
@@ -14,6 +18,7 @@ else
 {
     echo 'Update Successful';
 }
+
 /* Refresh the page after 2 seconds and return to the order.php page */
-header("refresh:2; url=order.php");
+header("refresh:10; url=order.php");
 ?>
