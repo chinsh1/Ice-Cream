05de9c772b9f5c38310f7804d322c7b87481a13f
chinsh1 <90356128+chinsh1@users.noreply.github.com>
2022-09-21 15:14:26 +1200

    Update insertcustomerv1.php

--- insertcustomerv1.php
+++ insertcustomerv1.php
@@ -8,10 +8,11 @@ $LName = $_POST['LName'];
 $Address = $_POST['Address'];
 $Phone = $_POST['Phone'];
 $Email = $_POST['Email'];
+$Password = $_POST['Password'];
 
 
-$insert_customer = "INSERT INTO customers (FName, LName, Address, Phone, Email) 
-            VALUES('$FName', '$LName', '$Address', '$Phone', '$Email')";
+$insert_customer = "INSERT INTO customers (FName, LName, Address, Phone, Email, Password) 
+            VALUES('$FName', '$LName', '$Address', '$Phone', '$Email', '$Password')";
 
 /* Check the data has been inserted */
 if(!mysqli_query($con, $insert_customer))
@@ -21,6 +22,7 @@ if(!mysqli_query($con, $insert_customer))
 else
 {
     echo 'Insert Successful';
+    echo .$row['CustomerID'].;
 }
 /* Refresh the page after 2 seconds and return to the drinks.php page */
 header("refresh:2; url=order.php");
