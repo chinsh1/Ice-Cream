b36a3ef0b230e3c6782477ceb16e21a2293b6e88
chinsh1 <90356128+chinsh1@users.noreply.github.com>
2022-10-03 19:49:19 +1300

    Update indexv3.php

--- indexv3.php
+++ indexv3.php
@@ -29,40 +29,45 @@ $all_flavours_result = mysqli_query($con, $all_flavours_query);
         </header>
     </div>
 
-    <!-- div for nav bar  -->
-    <div class="grid-items grid-item-2">
-        <nav>
-            <!-- links to other pages -->
-            <a class="nav-bar" href="index.php">HOME</a>
-            <a class="nav-bar" href="flavours.php">FLAVOURS</a>
-            <a class="nav-bar" href="order.php">ORDER</a>
-        </nav>
-    </div>
-
-    <!-- div for information -->
-    <div class = "grid-item-3">
-        <main>
+        <!-- div for nav bar  -->
+        <div class="grid-items grid-item-2">
+            <nav>
+                <!-- links to other pages -->
+                <a class="nav-bar" href="index.php">HOME</a>
+                <a class="nav-bar" href="flavours.php">FLAVOURS</a>
+                <a class="nav-bar" href="order.php">ORDER</a>
+            </nav>
+        </div>
+    <main>
+        <!-- div for information -->
+        <div class = "grid-item-3">
             <h2> About Us</h2>
             <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem
                 aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
             </p>
-
             <!-- when shop is open -->
             <h2> Open Hours </h2>
-            <p> open 24/7</p>
-    </div>
-    </main>
+            <b>Monday</b> - 12:00pm - 9:00pm <br>
+            <b>Tuesday</b> - CLOSED <br>
+            <b>Wednesday</b> - 12:00pm - 9:00pm <br>
+            <b>Thursday</b> - 12:00pm - 12:00am <br>
+            <b>Friday</b> - 12:00pm - 12:00am <br>
+            <b>Saturday</b> - 12:00pm - 12:00am <br>
+            <b>Sunday</b> - 12:00pm - 12:00am <br>
+        </div>
 
-    <div class = "grid-item-11">
-        <?php
-        echo "<img src='https://live.staticflickr.com/7134/7560180616_c1a580bcff_b.jpg' width='771px' height='571px' alt='Ice Cream Truck'/>";
-        ?>
-    </div>
+        <div class = "grid-item-11">
+            <?php
+            echo "<img src='https://live.staticflickr.com/7134/7560180616_c1a580bcff_b.jpg' width='771px' height='571px' alt='Ice Cream Truck'/>";
+            ?>
+        </div>
+    </main>
 
     <!-- div for footer -->
     <div class = "grid-items grid-item-4">
         <footer>
-            2022 Ice Cream - Shayla Chin
+            CONTACT US @ <br>
+            No Scream Ice Cream :) | 0220962340 | noscream@yahoo.com
         </footer>
     </div>
 </div>
