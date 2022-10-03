a9b878836256325ba4298fa926cb12f457153a01
chinsh1 <90356128+chinsh1@users.noreply.github.com>
2022-10-03 19:48:47 +1300

    Update flavoursv3.php

--- flavoursv3.php
+++ flavoursv3.php
@@ -19,6 +19,7 @@ $all_flavours_result = mysqli_query($con, $all_flavours_query);
 </head>
 
 <!-- grid for order and flavours page  -->
+{
 <div class="grid-container-2">
     <div class="grid-item-1">
         <header>
@@ -30,7 +31,6 @@ $all_flavours_result = mysqli_query($con, $all_flavours_query);
         </header>
     </div>
 
-
     <div class="grid-item-2">
         <nav>
             <!-- links to other pages -->
@@ -39,65 +39,64 @@ $all_flavours_result = mysqli_query($con, $all_flavours_query);
             <a class="nav-bar" href="order.php">ORDER</a>
         </nav>
     </div>
-
-        <div class="grid-item-8">
-            <!-- search bar for all food -->
-            <h2>Search a Item</h2>
-            <form action="flavours.php" method="post">
-                <input type="text" name='search'>
-                <input type="submit" name="submit" value="go!">
-            </form>
-            <!-- php for search bar -->
-            <?php
-            if (isset($_POST['search'])) {
-                $search = $_POST['search'];
-                // query to find item that is searched from database
-                $query1 = "SELECT * FROM flavours WHERE Name LIKE '%$search%'";
-                $query = mysqli_query($con, $query1);
-                $count = mysqli_num_rows($query);
-                //if input is invalid it will print this message
-                if ($count == 0) {
-                    echo "<p> There was no search results!";
-                } else {
-                    while ($row = mysqli_fetch_array($query)) {
-                        echo "<br>";
-                        echo $row ['Name'];
-                    }
+<main>
+    <div class="grid-item-8">
+        <!-- search bar for all food -->
+        <h2>Search a Item</h2>
+        <form action="flavours.php" method="post">
+            <input type="text" name='search'>
+            <input type="submit" name="submit" value="go!">
+        </form>
+        <!-- php for search bar -->
+        <?php
+        if (isset($_POST['search'])) {
+            $search = $_POST['search'];
+            // query to find item that is searched from database
+            $query1 = "SELECT * FROM flavours WHERE Name LIKE '%$search%'";
+            $query = mysqli_query($con, $query1);
+            $count = mysqli_num_rows($query);
+            //if input is invalid it will print this message
+            if ($count == 0) {
+                echo "<p> There was no search results!";
+            } else {
+                while ($row = mysqli_fetch_array($query)) {
+                    echo "<br>";
+                    echo $row ['Name'];
                 }
             }
+        }
+        ?>
+    </div>
+
+    <div class="grid-item-9">
+        <h2>Ice Cream Information</h2>
+        <table>
+            <tr>
+                <th>Flavour ID</th>
+                <th>Flavour Name</th>
+                <th>Cost</th>
+                <th>Stock</th>
+            </tr>
+            <?php
+            while($row = mysqli_fetch_array($all_flavours_result))
+            {
+                echo"<tr>";
+                echo"<td class='idColumn'> " .$row['FlavourID']. "</td>";
+                echo"<td class='flavourColumn'>" .$row['Name']. "</td>";
+                echo"<td class='costColumn'>" .$row['Cost']. "</td>";
+                echo"<td class='stockColumn'>" .$row['Stock']. "</td>";
+                echo"</tr>";
+            }
             ?>
-        </div>
 
-        <div class="grid-item-9">
-            <h2>Ice Cream Information</h2>
-            <table>
-                <tr>
-                    <th>Flavour ID</th>
-                    <th>Flavour Name</th>
-                    <th>Cost</th>
-                    <th>Stock</th>
-                </tr>
-
-                <?php
-                while($row = mysqli_fetch_array($all_flavours_result))
-                {
-                    echo"<tr>";
-                    echo"<td class='idColumn'> " .$row['FlavourID']. "</td>";
-                    echo"<td class='flavourColumn'>" .$row['Name']. "</td>";
-                    echo"<td class='costColumn'>" .$row['Cost']. "</td>";
-                    echo"<td class='stockColumn'>" .$row['Stock']. "</td>";
-                    echo"</tr>";
-                }
-                ?>
-
-            </table>
-        </div>
-
+        </table>
+    </div>
+</main>
+</div>
     <div class = "grid-item-10">
         <footer>
-            2022 Ice Cream - Shayla Chin
+            CONTACT US @ <br>
+            No Scream Ice Cream :) | 0220962340 | noscream@yahoo.com
         </footer>
     </div>
-    </body>
     </html>
-</div>
