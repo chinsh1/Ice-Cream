81eaa591e1c96ffc5f73ebdc03ce2af06fa533df
chinsh1 <90356128+chinsh1@users.noreply.github.com>
2022-10-04 12:53:36 +1300

    Update indexv3.php

--- indexv3.php
+++ indexv3.php
@@ -17,20 +17,20 @@ $all_flavours_result = mysqli_query($con, $all_flavours_query);
 </head>
 
 <body>
-<!-- grid container for home page  -->
-<div class="grid-container">
-    <div class="grid-items grid-item-1">
-        <header>
-            <?php
-            echo "<a class = 'title' href='index.php'>";
-            ?>
-            <!-- title of page -->
-            <h1> Ice Cream </h1>
-        </header>
-    </div>
+    <!-- grid container for home page  -->
+    <div class="grid-container">
+        <div class="grid-item-1">
+            <header>
+                <?php
+                echo "<a class = 'title' href='index.php'>";
+                ?>
+                <!-- title of page -->
+                <h1> Ice Cream </h1>
+            </header>
+        </div>
 
         <!-- div for nav bar  -->
-        <div class="grid-items grid-item-2">
+        <div class="grid-item-2">
             <nav>
                 <!-- links to other pages -->
                 <a class="nav-bar" href="index.php">HOME</a>
@@ -38,13 +38,12 @@ $all_flavours_result = mysqli_query($con, $all_flavours_query);
                 <a class="nav-bar" href="order.php">ORDER</a>
             </nav>
         </div>
-    <main>
+
         <!-- div for information -->
-        <div class = "grid-item-3">
+        <div class = "grid-items grid-item-3">
             <h2> About Us</h2>
-            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem
-                aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
-            </p>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem
+            aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
             <!-- when shop is open -->
             <h2> Open Hours </h2>
             <b>Monday</b> - 12:00pm - 9:00pm <br>
@@ -56,15 +55,14 @@ $all_flavours_result = mysqli_query($con, $all_flavours_query);
             <b>Sunday</b> - 12:00pm - 12:00am <br>
         </div>
 
-        <div class = "grid-item-11">
+        <div class = "grid-items grid-item-11">
             <?php
             echo "<img src='https://live.staticflickr.com/7134/7560180616_c1a580bcff_b.jpg' width='771px' height='571px' alt='Ice Cream Truck'/>";
             ?>
         </div>
-    </main>
 
     <!-- div for footer -->
-    <div class = "grid-items grid-item-4">
+    <div class = "grid-item-4">
         <footer>
             CONTACT US @ <br>
             No Scream Ice Cream :) | 0220962340 | noscream@yahoo.com
